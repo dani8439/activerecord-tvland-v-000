f1867b650c8f41f7f61bd0b4ee1ba7f6e0239618
Dani Schuhman <dani8439@aol.com>
2018-08-13 17:10:00 +0000

    automatically backed up by learn

--- db/migrate/004_create_characters.rb
+++ db/migrate/004_create_characters.rb
@@ -1,4 +1,8 @@
 class CreateCharacters < ActiveRecord::Migration[5.1]
-  def change 
+  def change
+    create_table :characters do |t|
+      t.string :first_name 
+      t.string :last_name 
+    end
   end
 end
